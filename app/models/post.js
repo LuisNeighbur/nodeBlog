ce6d7e08e9c33e1f8927cbb5b966294c599af9c3
LuisN <shaggikpo@gmail.com>
2014-09-19 22:48:28 -0300

    fix date default

--- app/models/post.js
+++ app/models/post.js
@@ -4,7 +4,7 @@ var PostSchema   = new Schema({
 	content: String,
 	title: String,
 	user: { type : Schema.ObjectId, ref : 'User' },
-	date: { type: Date, default: Date.now },
+	date: { type: Date, default: Date.now() },
 	date_update: {type: Date, default: null }
 });
 module.exports = mongoose.model('Post', PostSchema);
